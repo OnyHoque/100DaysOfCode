0a119aa86ec1853aef309c22bf04ccb362cc3c60
Ony Hoque <onyhoque@gmail.com>
2021-07-21 23:48:57 +0600

    Login folder

---
+++ login/app.js
@@ -0,0 +1,18 @@
+var obj = new Vue({
+    el: '#VueApp',
+    data: {
+        message: 'Hello Vue!',
+        username: '',
+        password: '',
+
+    },
+    methods: {
+        addToList: function() {
+
+        },
+    }
+})
+
+function clone(obj) {
+
+}
